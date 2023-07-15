cce3cfc4621981fbf764e2d07efe1c062466e922
paula hage <hage.paula@hotmail.com>
2023-07-15 19:34:04 +0200

    Created tests for the front-end project

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,5 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
 describe('Test AppComponent', () => {
-  it('should say Hello', () => {
-    const fixture = 
-  })
-})
+  it('should have string front', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    expect(app.title).toEqual('hello');
+  });
+
+  it('should have a h1 with Hello message', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const compiled = fixture.nativeElement;
+
+    expect(compiled.querySelector('h1').textContent).toBe('Hello');
+  });
+});
