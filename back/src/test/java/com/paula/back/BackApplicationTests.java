8051bd0201d4c1b5afacf7b27de929d3c506f2a5
paula hage <hage.paula@hotmail.com>
2023-07-15 17:20:45 +0200

    Making first test on backend

--- back/src/test/java/com/paula/back/BackApplicationTests.java
+++ back/src/test/java/com/paula/back/BackApplicationTests.java
@@ -1,5 +1,7 @@
 package com.paula.back;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,6 @@ class BackApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(1, 2);
 	}
-
 }
